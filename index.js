44cf44e8cfacdc63c3a73295634958d18bf998ed
carloslahrssen <carloslahrssen@gmail.com>
2018-03-24 16:22:28 -0400

    Implemented Mongo Stitch

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const Stitch = require('mongodb-stitch');
+let appId = 'hack-a-box-obkyj';
 
+Stitch.StitchClientFactory.create(appId)
+    .then(client => {
+        const db = client.service('mongodb', 'mongodb-atlas').db('Hackabull');
+        client.login().then(() =>
+            db.collection('Hackabox').find({}).limit(100).execute()
+                .then(docs => {
+                    console.log(docs);
+                })
+    )});
 
 
 app.get('/', (req, res) => {
     console.log("fuck we here");
-}):
+});
 
 app.listen(3000, ()=>{
     console.log('listening');
