9d681e3a223b5c2d08e5f3f5ab510f4763154bee
carloslahrssen <carloslahrssen@gmail.com>
2018-03-24 21:19:01 -0400

    Added HTML

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const app = express();
 const Stitch = require('mongodb-stitch');
+const exphbs = require('express-handlebars');
 let appId = 'hack-a-box-obkyj';
 
-Stitch.StitchClientFactory.create(appId)
-    .then(client => {
-        const db = client.service('mongodb', 'mongodb-atlas').db('Hackabull');
-        client.login().then(() =>
-            db.collection('Hackabox').find({}).limit(100).execute()
-                .then(docs => {
-                    console.log(docs);
-                })
-    )});
+const clientPromise = Stitch.StitchClientFactory.create('hack-a-box-obkyj');
+clientPromise.then(client => {
+    const db = client.service('mongodb', 'mongodb-atlas').db('Hackabull');
+    console.log("connected to mongo");
+});
+app.engine('handlebars', exphbs.create({}).engine)
+app.set('view engine', 'handlebars');
 
+app.post('/sendCode', (req, res) => {
+    let jsCode = req.body[0];
+
+
+});
 
 app.get('/', (req, res) => {
-    console.log("fuck we here");
+    res.render('home', {layout:false});
 });
 
-app.listen(3000, ()=>{
-    console.log('listening');
-});
+app.listen(3000);
